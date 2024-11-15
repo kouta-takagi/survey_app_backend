d0c05939e4e78d9a1c8ae154ce042852493b1cbb
kouta-takagi <k-takagi@ruby-dev.jp>
2024-11-15 15:53:08 +0900

    ログイン、ログアウト機能が完成（ui未実装）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+        include DeviseTokenAuth::Concerns::SetUserByToken
   def test
     render json: { message: "This is a test message from Rails" }
   end
